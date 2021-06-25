2c55284fb3e6bd5355cb7282a2cb1d44321b5084
Maxim King <73140396+MaximKing1@users.noreply.github.com>
2021-06-25 17:19:38 +0100

    first commit

---
+++ Test/index.js
@@ -0,0 +1,6 @@
+const { vtClient } = require('../main.js');
+
+const client = new vtClient("NUNYA");
+const data = client.scanInfo("cf4b367e49bf0b22041c6f065f4aa19f3cfe39c8d5abc0617343d1a66c6a26f5");
+
+console.log(data)
---
+++ src/index.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+
+const uploadFile = "https://www.virustotal.com/api/v3/files",
+    fileScanInfo = "https://www.virustotal.com/api/v3/files";
+
+class vtClient {
+    constructor(apiKey) {
+        this.key = apiKey;
+}
+
+    async scanFile(file) {
+        axios({
+            method: 'post',
+            url: uploadFile,
+            headers: { 'x-apikey': this.key }
+        }).then(function (response) {
+            return response;
+        }).catch(err => console.error(err.response.status));
+    }
+
+    async scanInfo(ID) {
+        await axios({
+            method: 'get',
+            url: `${fileScanInfo}/${ID}`,
+            headers: { 'x-apikey': this.key }
+        }).then(response => {
+            return console.log(response.data)
+        }).catch(err => console.error(err.response.status));
+    }
+
+}
+
+module.exports.vtClient = vtClient;
